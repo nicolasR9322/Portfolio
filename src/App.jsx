1f52d381abbbd1ef8acd5e6e91a117a9243ab11d
nicolasR9322 <nicolas.a.rodriguez93@gmail.com>
2023-09-04 17:54:15 -0300

    intento 3

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+    <h1>Hola, soy un portfolio</h1>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
